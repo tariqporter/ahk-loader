2d76029f62620c80fe3634d9274acc1a51af14f1
tariqporter <tic084@gmail.com>
2017-05-10 01:27:26 -0400

    Remove console.log and check if file exists

--- loader.js
+++ loader.js
@@ -10,11 +10,9 @@ function ahkLoader(source) {
 	const callback = this.async();
 	var re = /^([\t ])#Include\s+(.*)$/gim;
 	var re2 = /(^.*$)/gim;
-	var test = source.replace(/\r\n/gm, '\n');
 	var result = re.exec(source.replace(/\r\n/gm, '\n'));
-	console.log(test);
 	var innerSource = null;
-	while (result != null) {
+	while (result != null && fs.existsSync(result[2])) {
 		innerSource = fs.readFileSync(result[2], "utf-8");
 		innerSource = innerSource.replace(/\r\n/gm, '\n').replace(/^/gm, result[1]);
 		source = source.replace(result[0], innerSource);
