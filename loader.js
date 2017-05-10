3f1f6571d9491614cab9d17840a6cb58d78ddd57
tariqporter <tic084@gmail.com>
2017-05-10 02:19:26 -0400

    Separate getRecursiveSource into separate file

--- loader.js
+++ loader.js
@@ -3,35 +3,7 @@
 	Author Tariq Porter @tariqporter
 */
 
-const path = require("path");
-const fs = require("fs");
-
-function getRecursiveSource(module, source) {
-	const REGEX_INCLUDE = /^([\t ])#Include\s+(.*)$/gim;
-	let replaceNewLine = function(s) {
-		return s.replace(/\r\n/gm, '\n');
-	};
-	
-	source = replaceNewLine(source);
-	let result = REGEX_INCLUDE.exec(source);
-	
-	while (result != null) {
-		let match = result[0];
-		let indent = result[1];
-		let includePath = path.resolve(result[2]);
-		if (!fs.existsSync(includePath)) {
-			throw new Error("File does not exist: " + includePath);
-		}
-		
-		module.addDependency(includePath);
-		let includeSource = fs.readFileSync(includePath, "utf-8");
-		includeSource = getRecursiveSource(module, includeSource);
-		includeSource = replaceNewLine(includeSource).replace(/^/gm, indent);
-		source = source.replace(match, includeSource);
-		result = REGEX_INCLUDE.exec(source);
-	}
-	return source;
-}
+let getRecursiveSource = require("./getRecursiveSource.js");
 
 function ahkLoader(source) {
 	const self = this;
